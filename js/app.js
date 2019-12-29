a43a3c4d53a8e0996e7fb284f7d4f363d16c17d8
unknown <cristiantoong@gmail.com>
2019-12-29 21:57:36 +0800

    second commit

--- js/app.js
+++ js/app.js
@@ -17,3 +17,24 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     scrollTop: $($.attr(this, 'href')).offset().top
   }, 800);
 });
+
+
+// Scroll To Top
+
+$(document).ready(function() {
+  // show hide button on scroll
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.scrollToTop').fadeIn();
+      } else {
+          $('.scrollToTop').fadeOut();
+      }
+  });
+
+  // smooth scrolling to top
+  $('.scrollToTop').click(function() {
+      $('html,body').animate({
+          scrollTop: 0
+      })
+  })
+});
